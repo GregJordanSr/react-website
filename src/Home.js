3b435e2bccb5064e12bb57d7a1450460cbb3da62
Greg Jordan <gregljordan@gmail.com>
2019-06-25 17:08:56 -0400

    Made Navbar, Layout and Jumbotron components

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,14 @@ import React from 'react'
 const Home = () => (
     <div>
         <h2>HomePage</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi minima aperiam porro possimus, facilis numquam! Maiores molestiae nisi similique temporibus minus asperiores amet ab ipsam rem, id laborum dolorem?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel, facere rerum totam accusamus, rem distinctio esse eius excepturi aut quis quo dignissimos optio quasi necessitatibus fuga eligendi unde hic?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi minima aperiam porro possimus, facilis numquam! Maiores molestiae nisi similique temporibus minus asperiores amet ab ipsam rem, id laborum dolorem?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi minima aperiam porro possimus, facilis numquam! Maiores molestiae nisi similique temporibus minus asperiores amet ab ipsam rem, id laborum dolorem?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi minima aperiam porro possimus, facilis numquam! Maiores molestiae nisi similique temporibus minus asperiores amet ab ipsam rem, id laborum dolorem?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi minima aperiam porro possimus, facilis numquam! Maiores molestiae nisi similique temporibus minus asperiores amet ab ipsam rem, id laborum dolorem?</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum corrupti veritatis possimus sed pariatur rem placeat in dolor, consequatur corporis provident magni blanditiis repudiandae id omnis, illum accusamus atque perspiciatis?</p>
+        
+        <p>Epic cheeseburgers come in all kinds of manifestations, but we want them in and around our mouth no matter what. Slide those smashed patties with the gently caramelized meat fat between a toasted brioche bun and pass it over. You fall in love with the cheeseburger itself but the journey ain’t half bad either.</p>
+
+        <p>They’re the childhood friend that knows your highest highs and lowest lows. They’ve been with you through thick and thin and they’re the best at keeping secrets. Whether it’s dressed up or informal, cheeseburgers have your back.</p>
+
+        <p>Sometimes we lose sight of what really matters in life. There’s something to be said for a gourmet brie and truffle burger paired with parmesan frites, but don’t let that make you forget about the ol’ faithful with American cheddar and a squishy bun. Lettuce remind you that cheeseburgers come in all forms - bun intended.</p>
+
+        <p>Pop quiz: what’s the greatest thing to happen to your mind, body, and soul in recent history? A cheeseburger, obviously. Cheeseburgers know that what you want can also be what you need.</p>
     </div>
 )
         
